ab9abc934d8862162c30ae5a5185a0b334d93d7e
Deon <deonsummers91@gmail.com>
2019-10-23 01:55:44 -0500

    updates

--- copy-s3.sh
+++ copy-s3.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 pushd $DIR
 vim -c VimwikiAll2HTML -c q ~/vimwiki/index.wiki
-aws s3 sync . s3://deonsummers.com --delete
+aws s3 sync . s3://deonsummers.com --delete --exclude ".git/"
 git_sym add .
 git_sym commit -m "updates"
 git_sym push origin master
